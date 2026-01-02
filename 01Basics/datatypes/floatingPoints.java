ce1c37f4036817f5e4a13d68730de2e32829f72d
bhaveshCosmos <mail.bhaveshu@gmail.com>
2026-01-02 12:58:02 +0530

    update

---
+++ 01Basics/datatypes/floatingPoints.java
@@ -0,0 +1,12 @@
+package datatypes;
+
+public class floatingPoints {
+    public static void main(String[] args) {
+        // Floating-point
+        float num_float = 123.3139F;
+        double num_double = 1221.13213;
+
+        System.out.printf("float: %f\ndouble: %f", num_float, num_double);
+    }
+}
+
---
+++ 01Basics/datatypes/integers.java
@@ -0,0 +1,13 @@
+package datatypes;
+public class integers {
+    public static void main(String[] args) {
+        // Integer Types
+        byte num_byte = 127; // 1 byte
+        short num_short = 32767; // 2 byte
+        int num_int = 99999; // 4 byte
+        long num_long = 218938199; // 8 byte
+
+        System.out.printf("Byte: %d\nShort: %d\nInt: %d\nLong: %d", num_byte, num_short, num_int, num_long);
+    }
+    
+}
