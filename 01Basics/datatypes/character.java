ff2098ddf1f932ad7ca29355d537f4995e80e301
bhaveshCosmos <mail.bhaveshu@gmail.com>
2026-01-02 13:37:08 +0530

    "update"

---
+++ 01Basics/datatypes/character.java
@@ -0,0 +1,9 @@
+package datatypes;
+
+public class character {
+    public static void main(String[] args) {
+        // Character
+        char symbol = '#';
+        System.out.printf("Hashtag: %c", symbol);
+    }
+}
--- 01Basics/datatypes/floatingPoints.java
+++ 01Basics/datatypes/floatingPoints.java
@@ -3,10 +3,20 @@ package datatypes;
 public class floatingPoints {
     public static void main(String[] args) {
         // Floating-point
-        float num_float = 123.3139F;
-        double num_double = 1221.13213;
+        float num_float = 123.31392323F; // 4 byte
+        double num_double = 1221.1321322; // 8 bytes
+        System.out.printf("float: %f\ndouble: %f\n", num_float, num_double);
 
-        System.out.printf("float: %f\ndouble: %f", num_float, num_double);
+        // Implicit Type Conversion (Widening Conversion)
+        int num1;
+        num1 = 1000;
+        float floatNum = num1;
+        System.out.println("int to float: "+floatNum);
+
+        // Explicit Type Conversion (Narrowing / Type Casting)
+        double doubleNum = 221.21392732;
+        float numFloat = (float) doubleNum;
+        System.out.println("Double to float: " + numFloat);
     }
 }
 
